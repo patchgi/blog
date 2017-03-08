18ae8dea7c9be3e4e591a92ef395609f1f1f85f3
patchgi <pacchigi0910@gmail.com>
2017-03-08 22:08:36 +0900

    update Gemfile

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ require 'open-uri'
 require 'sinatra/json'
 require './models'
 
+ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || "sqlite3:db/development.db")
+
 enable :sessions
 
 get '/' do
